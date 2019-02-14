bb9064f20def54dd1017b11fd98756767b029d58
Angela Lim <angela.lim57@myhunter.cuny.edu>
2019-02-14 11:34:19 -0500

    updating + overload and updating comments

--- Project 1/points2.h
+++ Project 1/points2.h
@@ -117,7 +117,7 @@ class Points2 {
   // const version.
   // abort() if out-of-range.
   const std::array<Object, 2>& operator[](size_t location) const {
-    // Code missing.
+    //if the location is out of bounds abort
     if (location >= size_){
       std::abort();
     }
@@ -130,53 +130,50 @@ class Points2 {
  //  @return their sum. If the sequences are not of the same size, append the
  //    result with the remaining part of the larger sequence.
  friend Points2 operator+(const Points2 &c1, const Points2 &c2) {
-   // Code missing.
-   bool c1GreaterThanc2 = false;
      Points2 c3;
+     size_t min = 0;
      if(c1.size_ > c2.size_){
-       c1GreaterThanc2 = true;
        c3.size_ = c1.size_;
+       min = c2.size_;
      }
      else{
        c3.size_ = c2.size_;
+       min = c1.size_;
      }
+     //allocate a new array for c3 which will be the sum of c1 and c2
      c3.sequence_ = new std::array<Object, 2>[c3.size_];
-     size_t i = 0;
-     if(c1GreaterThanc2){
-       while(i < c2.size()){
-         c3.sequence_[i][0] = c1.sequence_[i][0] + c2.sequence_[i][0];
-         c3.sequence_[i][1] = c1.sequence_[i][1] + c2.sequence_[i][1];
-         i++;
-       }
-       while(i < c3.size()){
-         c3.sequence_[i][0] = c1.sequence_[i][0];
-         c3.sequence_[i][1] = c1.sequence_[i][1];
-         i++;
+     //add c1 and c2 to c3
+     for(size_t i = 0; i < min; i++){
+       c3.sequence_[i][0] = c1.sequence_[i][0] + c2.sequence_[i][0];
+       c3.sequence_[i][1] = c1.sequence_[i][1] + c2.sequence_[i][1];
+     }
+     //if c1 > c2 add the rest of c1 to c3
+     if(c1.size_ > c2.size_){
+       for(size_t j = 0; j < c3.size_; j++){
+         c3.sequence_[j][0] = c1.sequence_[j][0];
+         c3.sequence_[j][1] = c1.sequence_[j][1];
        }
      }
      else{
-       while(i < c2.size()){
-         c3.sequence_[i][0] = c1.sequence_[i][0] + c2.sequence_[i][0];
-         c3.sequence_[i][1] = c1.sequence_[i][1] + c2.sequence_[i][1];
-         i++;
-       }
-       while(i < c3.size()){
-         c3.sequence_[i][0] = c2.sequence_[i][0];
-         c3.sequence_[i][1] = c2.sequence_[i][1];
-         i++;
+       //if c2 > c1 add the rest of c2 to c3
+       for(size_t j = 0; j < c3.size_; j++){
+         c3.sequence_[j][0] = c2.sequence_[j][0];
+         c3.sequence_[j][1] = c2.sequence_[j][1];
        }
      }
+
      return c3;
 
  }
 
  // Overloading the << operator.
  friend std::ostream &operator<<(std::ostream &out, const Points2 &some_points2) {
-   // Code missing.
+   // if the size is 0 just return empty ()
    if(some_points2.size_ == 0){
      out << "()" << std::endl;
 				return out;
    }
+   //else return the sequence inside ()
    for(size_t i = 0; i < some_points2.size(); i++){
      out << "("<< some_points2.sequence_[i][0] << ", " << some_points2.sequence_[i][1] << ")";
 
